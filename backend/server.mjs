030db27da8d35635e24e429fa9e43deb45016952
Gustavo Henrique <gustavo.h.lima@iesb.edu.br>
2024-10-25 16:10:44 -0300

    atualzação

--- backend/server.mjs
+++ backend/server.mjs
@@ -22,14 +22,25 @@ app.get("/", (req, res) => {
  * DELETE/contatos/:id - deleta um contato
  */
 
-app.get('/contatos/:id', (req, res ) => {
+
+// Listar contatos
+app.get('/contatos', (req, res) => {
+    res.status(200).json({
+        error: false,
+        contatos
+    })
+})
+
+
+// Buscar contato por ID
+app.get('/contatos/:id', (req, res) => {
     const id = req.params.id;
     const sit = req.query.sit;
 
-    console.log (id, sit);
+    console.log(id, sit);
     const contato = contatos.find((contato) => contato.id == id);
     if (!contato) {
-        return res .status(404). json ({
+        return res.status(404).json({
             mensagem: "contato não encontrado"
         });
     }
@@ -37,12 +48,48 @@ app.get('/contatos/:id', (req, res ) => {
         id: id,
         nome: contato.nome
     });
-    
+
+});
+
+
+app.post('/contatos', (req, res) => {
+    const { nome, genero, telefone, email } = req.body;
+
+    if (!nome || !genero || !telefone || email)
+
+        return res.status(404).json({
+            error: true,
+            message: "mensagem inválida!"
+        });
+
+
+    if (contatos.find((contato) => contato.email === email))
+        return res.status(404).json({
+            error: true,
+            message: "email já cadastrado!"
+
+        });
+
+    id = (contatos.length == 0)
+        ? 1
+        : contatos[contatos.length - 1].id + 1;
+    const contato = { id: uuid(), nome, genero, telefone, email };
+    res.status(201).json({
+        error: false,
+        contato
+
     });
 
+})
 
-    app.post
+//Editar um contato
 
-    app.listen(3001, '127.0.0.1', () => {
-    console.log("servidor iniciado na porta 3001!")
+
+
+
+
+
+
+app.listen(3000, '127.0.0.1', () => {
+    console.log("servidor iniciado na porta 3000!")
 }); 
